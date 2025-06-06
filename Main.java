3de00aaf4a0767ac7613f11ca783284e8090e28d
mario.anguita <mario.ang.alvmaa@gmail.com>
2025-06-06 09:15:34 +0200

    cambio1

--- Main.java
+++ Main.java
@@ -9,6 +9,8 @@ public class Main {
         banco.getCliente("111A").ingresar(1000);
         banco.getCliente("111A").sacar(200);
         banco.traspasarDinero("111A", "222B", 300);
+        banco.traspasarDinero("111A", "222B", 0);
+
 
         banco.listarClientes();
         banco.getCliente("111A").ingresar(1000);
